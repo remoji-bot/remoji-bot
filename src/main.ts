3ce09b8f45da56254fe6a8024097e2bff0de93bc
Shino <shinotheshino@gmail.com>
2021-07-06 01:28:12 -0400

    ci: force trigger build

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ process.on("uncaughtException", error => {
   logger.error(error);
   process.exit(1);
 });
+
 process.on("unhandledRejection", rejection => {
   logger.error(rejection);
   process.exit(1);
