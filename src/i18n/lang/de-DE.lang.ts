af188a90729ca39edb2fadee430d711becd17c46
Shino <shinotheshino@gmail.com>
2021-07-09 16:33:30 -0400

    fix: resolve merge conflicts

--- src/i18n/lang/de-DE.lang.ts
+++ src/i18n/lang/de-DE.lang.ts
@@ -18,7 +18,7 @@
 
 import { PermissionString } from "discord.js";
 import { I18NLanguage } from "..";
-import { Nullable } from "../../core/utils/types";
+import { Nullable } from "@remoji-bot/core";
 import { Lang_en_US } from "./en-US.lang";
 
 /**
