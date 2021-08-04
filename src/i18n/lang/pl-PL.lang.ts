e968db3c1b37d2b3e7db71f9f025d6bf3e7499a0
tipakA <emka456@onet.pl>
2021-08-04 05:24:20 +0200

    feat: update translation

--- src/i18n/lang/pl-PL.lang.ts
+++ src/i18n/lang/pl-PL.lang.ts
@@ -18,20 +18,19 @@ export class Lang_pl_PL extends I18N {
 
 	public override ping_success = (delay: number): string => `Pong! Opóźnienie: ${delay}ms`;
 
-	// TODO: image
 	public override image_download_error_with_reason = (reason: Error): string =>
-		`Nie udało się pobrać obrazu: \`${reason.message}\``;
+		`Nie udało się pobrać obrazka: \`${reason.message}\``;
 	// TODO: usage? this name doesn't look valid / this doens't look like valid name
 	public override image_invalid_name = 'Ta nazwa nie wygląda na poprawną...';
 	// TODO: same as above
 	public override image_invalid_url = 'Ten adres URL nie wygląda na poprawny...';
 	public override image_invalid_domain =
-		'Ten obraz nie jest umieszczony na dozwolonej stronie. Spróbuj użyć imgur.com albo wyślij jako załącznik na Discordzie!';
-	public override image_unknown_error = 'Nie udało się pobrać obrazu z powodu nieznanego błędu.';
+		'Ten obrazek nie jest umieszczony na dozwolonej stronie. Spróbuj użyć imgur.com albo wysłać jako załącznik na Discordzie!';
+	public override image_unknown_error = 'Nie udało się pobrać obrazka z powodu nieznanego błędu.';
 	public override image_upload_success = (name: string, emoji: string): string =>
 		`:tada:  Przesłano \`:${name}:\` na Twój serwer! ${emoji}`;
 	public override image_upload_failed_unknown_error =
-		'Nie udało się przesłać emoji z powodu nieznanego błędu. Spróbuj użyć innego obrazu lub wysłać obraz jako załącznik na Discordzie.';
+		'Nie udało się przesłać emoji z powodu nieznanego błędu. Spróbuj użyć innego obrazka lub wysłać obrazek jako załącznik na Discordzie.';
 	public override language_change_success = (oldLocale: Nullable<I18NLanguage>, newLocale: I18NLanguage): string =>
 		`Pomyślnie zmieniono język${oldLocale ? ` z \`${oldLocale}` : ''} na \`${newLocale}\``;
 
@@ -54,11 +53,9 @@ export class Lang_pl_PL extends I18N {
 
 	public override emote_copy_invalid_emote =
 		'Nie znaleziono emoji do skopiowania. Można kopiować tylko **niestandardowe** emoji.';
-	// TODO: emoji? emote?
 	public override emote_copy_invalid_name =
 		'To nie jest poprawna nazwa dla emoji. Nazwa musi mieć od 2 do 32 znaków oraz składać się tylko z liter, cyfr lub znaków podkreślenia';
-	// TODO: ?
-	public override emote_copy_invalid_url = 'Nie udało się pobrać emoji. Czy na pewno nie została usunięta?';
+	public override emote_copy_invalid_url = 'Nie udało się pobrać emoji. Czy na pewno nie zostało usunięte?';
 	public override emote_copy_invalid_domain =
 		'Nie udało się pobrać emoji z powodu niepoprawnego adresu CDN. Prosimy o zgłoszenie tego błędu deweloperom.';
 	public override emote_copy_unknown_download_error = 'Podczas pobierania emoji wystąpił nieznany błąd.';
@@ -69,10 +66,8 @@ export class Lang_pl_PL extends I18N {
 	public override emote_copy_multiple_success = (failed: number, success: number): string =>
 		`Pomyślnie przesłano ${success} emoji! (Niepomyślne: ${failed})`;
 
-	// TODO: ????
-	// TODO: Uh, you do know that managing emoji is done in discord anyway, and that since 8.45g from march 2019 android has ways to manage emotes as well, right?
 	public override info_remoji_description =
-		'Remoji to bardzo prosty lecz bardzo skuteczny (?) bot do zarządzania emoji na Discordzie. Potrafi między innymi ~~kraść~~ *kopiować* lub przesyłać emoji na Twój serwer bezpośrednio w Discordzie, nawet będąc na telefonie!';
+		'Remoji to bardzo prosty lecz bardzo potężny bot do zarządzania emoji na Discordzie. Potrafi między innymi ~~kraść~~ *kopiować* lub przesyłać emoji na Twój serwer bezpośrednio w Discordzie, nawet będąc na telefonie!';
 	public override info_remoji_server_field = 'Dołącz do serwera Remoji aby być na bieżąco z nowymi funkcjami!';
 	public override info_remoji_server_invite = (invite: string): string => `**[DOŁĄCZ DO SERWERA REMOJI](${invite})**`;
 	public override info_remoji_bot_field = 'Dodaj Remoji!';
